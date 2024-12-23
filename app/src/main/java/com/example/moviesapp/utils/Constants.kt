84cc537c735e203f774b3b5a085f403fc1e64cdb
shankkow <shashankssk95@gmail.com>
2024-12-23 23:57:49 +0530

    Securely integrated TMDB API key using local.properties and BuildConfig

--- app/src/main/java/com/example/moviesapp/utils/Constants.kt
+++ app/src/main/java/com/example/moviesapp/utils/Constants.kt
@@ -1,7 +1,9 @@
 package com.example.moviesapp.utils
 
+import com.example.moviesapp.BuildConfig
+
 object Constants {
-    const val API_KEY = "723328061601fa69f1478cf6a46beaac"
+    const val API_KEY = BuildConfig.TMDB_API_KEY // Securely fetched from BuildConfig
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
 }
